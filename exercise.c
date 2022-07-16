28ab9466c9249aff4692e807b136d62d0ea7ae72
Stephen <59405071+I-Program-Stuff@users.noreply.github.com>
2022-07-15 20:51:09 -0400

    Create exercise.c

---
+++ exercise.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#define ASCII_DISTANCE 55
+
+int htoi(char[]);
+
+int main(void){
+    printf("Decimal Value: %d\n",htoi("0xFF"));
+    return 0;
+}
+
+int htoi(char s[]){
+    int i,inc,res,v;
+	res = 0;
+    for(i=strlen(s)-1,inc=1;(s[i] >= '0' && s[i] <= '9') || (s[i] >= 'A' && s[i] <= 'F');i--,inc*=16){
+        printf("%d\n",inc);
+        if(s[i] >= '0' && s[i] <= '9'){
+            v = (s[i] - '0') * inc;
+			res += v;
+        }else{
+			char currentChar = s[i];
+			v = (currentChar - ASCII_DISTANCE) * inc;
+			res += v;
+		}
+    }
+    return res;
+}
